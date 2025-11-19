6bc26c6f01101c22184a980915ee0d0bc65681a6
sumerjakhar <sumer.jakhar@emoha.com>
2025-11-19 17:53:11 +0530

    added session flow

--- crates/e2e_tests/src/lib.rs
+++ crates/e2e_tests/src/lib.rs
@@ -1,15 +1,14 @@
-pub mod login;
-pub mod create_lead;
 pub mod create_invoice;
+pub mod create_lead;
+pub mod login;
 pub mod utils {
-    pub mod drop_down_select;
     pub mod dashboard_button;
+    pub mod drop_down_select;
     pub mod retry;
 }
 use std::time::Duration;
 
 use anyhow::Result;
-
 
 pub async fn run() -> Result<()> {
     let cfg = lib_test_helpers::config::get_config();
--- crates/e2e_tests/src/login.rs
+++ crates/e2e_tests/src/login.rs
@@ -1,21 +1,22 @@
-use thirtyfour::prelude::*;
-use std::{time::Duration};
-use lib_test_helpers::{driver::global_driver, session_file::save_session};
 use crate::utils::retry::retry;
-
-pub async fn login_test(base_url:&str) -> WebDriverResult<()> {
+use lib_test_helpers::config::get_config;
+use lib_test_helpers::{driver::global_driver, session_file::save_session};
+use std::time::Duration;
+use thirtyfour::prelude::*;
+pub async fn login_test(base_url: &str) -> WebDriverResult<()> {
     let driver = global_driver().await?;
     driver.goto(base_url).await?;
+    let cfg = get_config();
 
     driver
         .find(By::Id("basic_email"))
         .await?
-        .send_keys("wmstest@emoha.com")
+        .send_keys(&cfg.email)
         .await?;
     driver
         .find(By::Id("basic_password"))
         .await?
-        .send_keys("123456")
+        .send_keys(&cfg.password)
         .await?;
     driver
         .find(By::Css("button[type='submit']"))
@@ -26,37 +27,42 @@ pub async fn login_test(base_url:&str) -> WebDriverResult<()> {
     retry(
         || async {
             driver
-            .query(By::XPath("//input[@placeholder='Enter Phone..']"))
-            .wait(Duration::from_secs(50), Duration::from_millis(500))
-            .first()
-            .await?
-            .send_keys("9501285590")
-            .await
+                .query(By::XPath("//input[@placeholder='Enter Phone..']"))
+                .wait(Duration::from_secs(50), Duration::from_millis(500))
+                .first()
+                .await?
+                .send_keys(&cfg.phone)
+                .await
         },
         5,
         400,
-    ).await?;
+    )
+    .await?;
 
     driver
         .query(By::XPath("//input[@placeholder='Enter Phone..']"))
         .wait(Duration::from_secs(50), Duration::from_millis(500))
         .first()
         .await?
-        .send_keys("9501285590")
+        .send_keys(&cfg.phone)
         .await?;
     driver
         .query(By::Css("button.otp-send-btn"))
         .wait(Duration::from_secs(10), Duration::from_millis(500))
         .first()
         .await?
-        .click().await.ok();   
+        .click()
+        .await
+        .ok();
 
     driver
-        .query(By::XPath("//input[@placeholder='Enter 4-digit OTP' and not(@disabled)]"))
+        .query(By::XPath(
+            "//input[@placeholder='Enter 4-digit OTP' and not(@disabled)]",
+        ))
         .wait(Duration::from_secs(10), Duration::from_millis(500))
         .first()
         .await?
-    .send_keys("0180")
+        .send_keys(&cfg.otp)
         .await?;
     driver
         .find(By::Css("button.enter-otp-verify-button-visible"))
@@ -71,9 +77,14 @@ pub async fn login_test(base_url:&str) -> WebDriverResult<()> {
         .click()
         .await?;
 
-    tokio::time::sleep(Duration::from_millis(500)).await;
-    let _ =save_session(&driver, "session_daily").await;
-    
+    driver
+        .query(By::XPath("//a[contains(., 'Create Lead')]"))
+        .wait(Duration::from_secs(10), Duration::from_millis(500))
+        .first()
+        .await?;
+
+    let _ = save_session(&driver, "session_daily").await;
+
     // create_lead::create_lead_test(&driver).await?;
 
     println!("✅ Login test passed!");
