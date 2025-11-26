2ebdd8ee5e8f892d775096a3ef2940a22d587212
sumerjakhar <sumer.jakhar@emoha.com>
2025-11-26 14:21:44 +0530

    added-new

--- crates/e2e_tests/src/create_invoice.rs
+++ crates/e2e_tests/src/create_invoice.rs
@@ -4,9 +4,15 @@ use thirtyfour::prelude::*;
 
 pub async fn create_invoice(driver: WebDriver, base_url: String) -> WebDriverResult<()> {
     let _ = restore_session(&driver, "session_daily", &base_url).await;
+    // driver.goto(&base_url).await?;
+    let cfg = lib_test_helpers::config::get_config();
 
-    let url = format!("{}/elders", base_url);
-    driver.goto(&url).await?;
-    select_dashboard(&driver, By::XPath("//a[contains(., 'elders')]")).await?;
+    select_dashboard(
+        &driver,
+        By::XPath("/html/body/div[1]/div[4]/div/div/aside/div/ul/li[6]/a"),
+    )
+    .await?;
+    driver.goto(format!("/details{}", cfg.elder_number)).await?;
+
     Ok(())
 }
